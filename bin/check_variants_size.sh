00f443a044d2cfe37da6cf87fb47afc8a93b0db2
Kübra Narcı <kbrnrc@gmail.com>
2022-07-20 14:47:33 +0200

    somatic functional file check added

--- bin/check_variants_size.sh
+++ bin/check_variants_size.sh
@@ -1,7 +1,7 @@
 
 set -euo pipefail
 
-usage() { echo "Usage: $0 [-i vcf_file] [-m max_screenshot]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-i vcf_file] [-v max_screenshot] [-t tumor] [-c control] [-r ref] [-w window] [-m repeat_masker] [-s iscontrol] [-o screenshots]" 1>&2; exit 1; }
 
 while [[ $# -gt 0 ]]
 do
@@ -12,8 +12,43 @@ do
 			shift # past argument
 	    shift # past value
 			;;
-		-m)
+		-v)
 			MAX_VARIANT_SCREENSHOTS=$2
+			shift # past argument
+	    shift # past value
+			;;
+		-t)
+			FILENAME_TUMOR_BAM=$2
+			shift # past argument
+	    shift # past value
+			;;
+		-c)
+			FILENAME_CONTROL_BAM=$2
+			shift # past argument
+	    shift # past value
+			;;
+		-r)
+			REFERENCE_GENOME=$2
+			shift # past argument
+	    shift # past value
+			;;
+		-w)
+			WINDOW_SIZE=$2
+			shift # past argument
+	    shift # past value
+			;;
+		-m)
+			REPEAT_MASKER=$2
+			shift # past argument
+	    shift # past value
+			;;
+		-s)
+			isControlWorkflow=$2
+			shift # past argument
+	    shift # past value
+			;;
+		-o)
+			combined_screen_shots=$2
 			shift # past argument
 	    shift # past value
 			;;
@@ -24,9 +59,34 @@ functional_var_count=`cat ${somatic_functional_indel_vcf} | tail -n +2 | wc -l |
 
 if [[ $functional_var_count -eq 0 ]]; then
     printf "WARNING: No functional variants present in ${somatic_functional_indel_vcf}\n"
+        > "$combined_screen_shots"
 
 elif [[ $functional_var_count -le $MAX_VARIANT_SCREENSHOTS ]]; then
-	printf "Functional variants will be presented\n"
+
+    if [[ "${isControlWorkflow}" == true ]]; then
+        visualize.py \
+            --vcf=${somatic_functional_indel_vcf} \
+            --control=${FILENAME_CONTROL_BAM} \
+            --tumor=${FILENAME_TUMOR_BAM} \
+            --ref=${REFERENCE_GENOME} \
+            --prefix=indel_ \
+            --window=${WINDOW_SIZE} \
+            --annotations=${REPEAT_MASKER}
+    fi
+
+    if [[ "${isControlWorkflow}" == false ]]; then
+        visualize.py \
+            --vcf=${somatic_functional_indel_vcf} \
+            --tumor=${FILENAME_TUMOR_BAM} \
+            --ref=${REFERENCE_GENOME} \
+            --prefix=_indel \
+            --window=${WINDOW_SIZE} \
+            --annotations=${REPEAT_MASKER}
+    fi
+    pngs=(`ls *.pdf`)
+    sorted=$(printf "%s\n" ${pngs[@]}|sort -k1,1V)
+
+    gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=${combined_screen_shots} ${sorted}
 else
     printf "WARNING: No screenshots done, more than $MAX_VARIANT_SCREENSHOTS (cvalue - MAX_VARIANT_SCREENSHOTS) functional variants present in ${somatic_functional_indel_vcf}\n"
 
