44a04bacec913931d1144642cf9878aeb894430e
Kübra Narcı <kbrnrc@gmail.com>
2022-08-11 18:42:58 +0300

    run in the cluster

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-module load anaconda3/2021.05
-conda activate /dkfz/cluster/virtualenvs/paramasi/nf
-nextflow run main.nf -profile dkfz_cluster,singularity -resume
+nextflow run main.nf -profile dkfz_cluster,singularity
