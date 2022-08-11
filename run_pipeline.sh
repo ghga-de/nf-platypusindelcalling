3eb24ca1f5df4f7f159f207166b72172734c8946
Kübra Narcı <kbrnrc@gmail.com>
2022-08-11 13:44:28 +0300

    nextflow version change

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-module load nextflow/21.10.6
-nextflow run main.nf -profile dkfz_cluster,singularity --runTinda false
+module load anaconda3/2021.05
+conda activate /dkfz/cluster/virtualenvs/paramasi/nf
+nextflow run main.nf -profile dkfz_cluster,singularity -resume
