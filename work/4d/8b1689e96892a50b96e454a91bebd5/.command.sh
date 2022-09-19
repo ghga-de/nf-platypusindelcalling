3bd37636db8fd12e68e6fc0902d963c757d1dec8
Kübra Narcı <kbrnrc@gmail.com>
2022-09-19 15:52:43 +0200

    Delete work directory

--- work/4d/8b1689e96892a50b96e454a91bebd5/.command.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -euo pipefail
-check_samplesheet.py \
-    samplesheet_cluster.csv \
-    samplesheet.valid.csv
-
-cat <<-END_VERSIONS > versions.yml
-"NF_PLATYPUSINDELCALLING:PLATYPUSINDELCALLING:INPUT_CHECK:SAMPLESHEET_CHECK":
-    python: $(python --version | sed 's/Python //g')
-END_VERSIONS
