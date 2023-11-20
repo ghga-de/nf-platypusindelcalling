040da32362e89c87957c927d7740d6cedcbb06cf
kubranarci <kbrnrc@gmail.com>
2023-11-20 12:07:51 +0100

    input check fixed

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 module load nextflow/22.07.1-edge
-nextflow run main.nf -profile dkfz_cluster_38,singularity
+nextflow run main.nf -profile dkfz_cluster_38,singularity --input assets/test.csv
